7fc1d66581e380cd777eee41f6500711a8d91d5e
nicolasmcn <n.mancino03@gmail.com>
2025-03-04 17:01:51 +0100

    $NF for last place

--- Re_Fundamentals/get_entry_point.sh
+++ Re_Fundamentals/get_entry_point.sh
@@ -23,7 +23,7 @@ fi
 # Extraction des informations avec readelf
 magic_number=$(hexdump -n 16 -e '16/1 "%02x "' "$file_name")
 class=$(readelf -h "$file_name" | grep "Class:" | awk '{print $2, $3}')
-byte_order=$(readelf -h "$file_name" | grep "Data:" | awk '{print $3, $4}')
+byte_order=$(readelf -h "$file_name" | grep "Data:" | awk '{print $NF-1, $NF}')
 entry_point_address=$(readelf -h "$file_name" | grep "Entry point address:" | awk '{print $4}')
 
 # Inclure messages.sh pour formater l'affichage
