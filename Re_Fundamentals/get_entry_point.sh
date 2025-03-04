615752b2692ade561aea4999d753ece3bf80a0dd
nicolasmcn <n.mancino03@gmail.com>
2025-03-04 17:10:14 +0100

    without spaces

--- Re_Fundamentals/get_entry_point.sh
+++ Re_Fundamentals/get_entry_point.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Véri si un argument est fourni
+# Vérifier si un argument est fourni
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <ELF file>"
     exit 1
@@ -8,27 +8,27 @@ fi
 
 file_name="$1"
 
-# Vérfication si le fichier existe
+# Vérifier si le fichier existe
 if [ ! -f "$file_name" ]; then
     echo "Error: File '$file_name' not found!"
     exit 1
 fi
 
-# Vérf si le fichier est un ELF
+# Vérifier si le fichier est un ELF
 if ! file "$file_name" | grep -q "ELF"; then
     echo "Error: '$file_name' is not an ELF file!"
     exit 1
 fi
 
-# Extraction des informations avec readelf
-magic_number=$(hexdump -n 16 -e '16/1 "%02x "' "$file_name")
-class=$(readelf -h "$file_name" | grep "Class:" | awk '{print $2, $3}')
-byte_order=$(readelf -h "$file_name" | grep "Data:" | awk '{print $(NF-1), $NF}' | sed 's/,//g' | xargs)
+# Extraire les informations avec readelf en supprimant les espaces
+magic_number=$(hexdump -n 16 -e '16/1 "%02x "' "$file_name" | sed 's/ $//')
+class=$(readelf -h "$file_name" | grep "Class:" | awk '{print $2}' | tr -d ' ')
+byte_order=$(readelf -h "$file_name" | grep "Data:" | awk '{print $(NF-1), $NF}' | sed 's/,//g' | tr -s ' ')
 entry_point_address=$(readelf -h "$file_name" | grep "Entry point address:" | awk '{print $4}')
 
 # Inclure messages.sh pour formater l'affichage
 source messages.sh
 
-# Appel de fonction pour afficher les résultats
+# Appeler la fonction pour afficher les résultats
 display_elf_header_info
 
